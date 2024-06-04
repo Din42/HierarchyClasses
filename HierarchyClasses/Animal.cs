09be216b6218f2d668f8592ae96750009251340d
Denis <fox1086@g,ail.com>
2024-06-04 22:31:19 +0700

    Реализация интерфейса в классе Animal

--- HierarchyClasses/Animal.cs
+++ HierarchyClasses/Animal.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HierarchyClasses
 {
-    internal class Animal
+    internal class Animal : IAnimal
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public abstract void Sound();        
     }
 }
