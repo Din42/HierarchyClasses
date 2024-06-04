19e289fbdbd9c2bdc9ea2fe730fdfb2068d2c32e
Denis <fox1086@g,ail.com>
2024-06-04 22:38:57 +0700

    Удалины лишние библиотеки, создан класс Cat, и переделал класс Animal в абстрактный.

---
+++ HierarchyClasses/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HierarchyClasses
+{
+    internal class Cat : Animal
+    {
+        public int Eats { get; set; }
+        public int Pows { get; set; }
+        public int Tail {  get; set; }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
