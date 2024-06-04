77e1c76e6c46e7faaf2cf91681e28ebe58654f8d
Denis <fox1086@g,ail.com>
2024-06-04 22:27:55 +0700

    Добавление интерфейса

---
+++ HierarchyClasses/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchyClasses
+{
+    internal interface IAnimal
+    {
+        string Name { get; set; }
+        int Age {  get; set; }
+        void Sound();
+    }
+}
