f9125799f62381a0e713b7b0ac6feda16ecda7c2
Denis <fox1086@g,ail.com>
2024-06-05 09:18:01 +0700

    Добавил классы Cat и Brid

---
+++ HierarchyClasses/Bird.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HierarchyClasses
+{
+    internal class Bird : Animal
+    {
+        public int Wings { get; set; }
+        public int Tail { get; set; }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Чирик");
+        }
+    }
+}
---
+++ HierarchyClasses/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HierarchyClasses
+{
+    internal class Cat : Animal
+    {
+        public int Ears { get; set; }
+        public int Paws { get; set; }
+        public int Tail { get; set; }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
