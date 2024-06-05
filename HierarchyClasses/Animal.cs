813f0f9d2f95bd4d7cf312543f5ec07008e17835
Denis <fox1086@g,ail.com>
2024-06-05 09:13:49 +0700

    переделал класс Animal в абстрактный

--- HierarchyClasses/Animal.cs
+++ HierarchyClasses/Animal.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HierarchyClasses
 {
-    internal class Animal : IAnimal
+    abstract class Animal : IAnimal
     {
         public string Name { get; set; }
         public int Age { get; set; }
