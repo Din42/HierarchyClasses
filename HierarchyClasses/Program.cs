3a87922ab8de8956e6f72bf7c10c1635ee636419
Denis <fox1086@g,ail.com>
2024-06-05 09:22:10 +0700

    Вывод

--- HierarchyClasses/Program.cs
+++ HierarchyClasses/Program.cs
@@ -6,6 +6,11 @@ namespace HierarchyClasses
     {
         static void Main(string[] args)
         {
+            Cat cat = new Cat {Name = "Мурсик", Age = 2, Ears = 2, Paws = 4, Tail = 1 };
+            Bird bird = new Bird { Name = "Чижик", Age = 1, Tail = 2, Wings = 1 };
+
+            cat.Sound();
+            bird.Sound();
         }
     }
 }
